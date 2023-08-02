45568a7e1cf0ed5425a8d3530438275fa00792ab
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-08-02 18:59:17 +0300

    task-4

--- task-4.sql
+++ task-4.sql
@@ -1,1 +1,6 @@
 -- YOUR SOLUTION HERE
+SELECT DISTINCT customers.company_name AS company_name, us_states.state_name AS state_name
+FROM customers
+INNER JOIN orders ON customers.customer_id=orders.customer_id
+JOIN us_states ON orders.ship_region=us_states.state_abbr
+ORDER BY company_name;
