d7a0cec1e38e1b76d01bbdc679f465ef050f6f03
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-08-02 18:31:57 +0300

    task-3

--- task-1.sql
+++ task-1.sql
@@ -1,5 +1,5 @@
 -- YOUR SOLUTION HERE
 SELECT products.product_name AS product, suppliers.company_name AS company
 FROM products
-         JOIN suppliers ON products.supplier_id = suppliers.supplier_id
+JOIN suppliers ON products.supplier_id = suppliers.supplier_id
 ORDER BY product, company;
--- task-3.sql
+++ task-3.sql
@@ -1,1 +1,6 @@
 -- YOUR SOLUTION HERE
+SELECT categories.category_name AS category, COUNT(products.product_id) AS number_of_products
+FROM categories
+LEFT JOIN products ON categories.category_id = products.category_id
+GROUP BY categories.category_name
+ORDER BY number_of_products DESC, category;
