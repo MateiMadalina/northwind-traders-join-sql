81c8a813fd53f779f8f3f668be11270478b1186e
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-08-02 18:24:08 +0300

    task-2

--- task-2.sql
+++ task-2.sql
@@ -1,1 +1,6 @@
 -- YOUR SOLUTION HERE
+SELECT orders.order_id, freight, order_details.discount
+FROM orders
+JOIN order_details ON orders.order_id = order_details.order_id
+WHERE discount > 0
+ORDER BY freight;
