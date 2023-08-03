87085e38536dbe8ad8c94eddf41cf533c2774709
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-08-03 11:47:36 +0300

    task-5

--- task-5.sql
+++ task-5.sql
@@ -1,1 +1,7 @@
 -- YOUR SOLUTION HERE
+SELECT COUNT(employees.employee_id) AS count, region.region_description
+FROM employees
+JOIN employee_territories ON employees.employee_id = employee_territories.employee_id
+JOIN territories ON employee_territories.territory_id = territories.territory_id
+JOIN region ON territories.region_id = region.region_id
+GROUP BY region.region_description;
