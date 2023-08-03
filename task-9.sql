fdea4cebaf89bb07cd0a5d57b12dae4449fd2a22
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-08-03 12:32:34 +0300

    task-9

--- task-9.sql
+++ task-9.sql
@@ -1,1 +1,9 @@
 -- YOUR SOLUTION HERE
+SELECT products.product_name,CAST(SUM(order_details.quantity * order_details.unit_price) AS INTEGER) AS sum_price
+FROM order_details
+JOIN products ON order_details.product_id = products.product_id
+JOIN orders ON order_details.order_id = orders.order_id
+WHERE EXTRACT(YEAR FROM orders.order_date) = 1997
+GROUP BY products.product_name
+ORDER BY sum_price DESC
+LIMIT 10;
