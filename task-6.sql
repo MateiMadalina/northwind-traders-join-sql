fb95a99c1c4fa0e83c03c2b6da98efc2dcce74d2
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-08-03 12:00:55 +0300

    task-6

--- task-6.sql
+++ task-6.sql
@@ -1,1 +1,7 @@
 -- YOUR SOLUTION HERE
+SELECT CONCAT(title_of_courtesy, ' ', first_name, ' ', last_name) AS employee,
+COUNT(orders.employee_id) AS orders
+FROM employees
+JOIN orders ON employees.employee_id = orders.employee_id
+GROUP BY employees.title_of_courtesy, employees.first_name, employees.last_name
+ORDER BY COUNT(orders.employee_id) DESC;
