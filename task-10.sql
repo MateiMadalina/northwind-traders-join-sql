7afb760b4419500e968f99682feb988b1761961b
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-08-03 12:45:50 +0300

    task-10

--- task-10.sql
+++ task-10.sql
@@ -1,1 +1,13 @@
 -- YOUR SOLUTION HERE
+SELECT
+    EXTRACT(YEAR FROM orders.order_date) AS year,
+    EXTRACT(MONTH FROM orders.order_date) AS month,
+    COUNT(orders.order_id) AS order_count,
+    CAST(SUM(order_details.quantity * (order_details.unit_price - order_details.discount * order_details.unit_price)) AS INTEGER) AS revenue
+FROM orders
+    JOIN order_details ON orders.order_id = order_details.order_id
+WHERE EXTRACT(YEAR FROM orders.order_date) = 1997
+GROUP BY
+    EXTRACT(YEAR FROM orders.order_date),
+    EXTRACT(MONTH FROM orders.order_date)
+ORDER BY year, month;
