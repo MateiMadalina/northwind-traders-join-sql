76630ed372cf83b138674e2959b683e4b3c476ba
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-08-03 12:08:02 +0300

    task-7

--- task-7.sql
+++ task-7.sql
@@ -1,1 +1,5 @@
 -- YOUR SOLUTION HERE
+SELECT products.product_name, suppliers.company_name, suppliers.contact_name
+FROM products
+JOIN suppliers ON products.supplier_id = suppliers.supplier_id
+ORDER BY products.product_name;
